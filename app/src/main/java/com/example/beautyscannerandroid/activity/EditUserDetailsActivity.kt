c77008021a2b94d9bd822e078e6c342dc9daec27
aksuz <zuzanna.burzynska@interia.pl>
2020-08-17 17:38:14 +0200

    flow(done): edit user details and edit user password (without saving)

--- app/src/main/java/com/example/beautyscannerandroid/activity/EditUserDetailsActivity.kt
+++ app/src/main/java/com/example/beautyscannerandroid/activity/EditUserDetailsActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.example.beautyscannerandroid.R
 
 class EditUserDetailsActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_user_details)
--- app/src/main/java/com/example/beautyscannerandroid/fragment/UserDetailsFragment.kt
+++ app/src/main/java/com/example/beautyscannerandroid/fragment/UserDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.beautyscannerandroid.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,10 +10,11 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.beautyscannerandroid.R
-import com.example.beautyscannerandroid.model.User
+import com.example.beautyscannerandroid.activity.EditUserDetailsActivity
+import com.example.beautyscannerandroid.activity.EditUserPasswordActivity
 import com.example.beautyscannerandroid.viewmodel.UserViewModel
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_user_details.*
+import kotlinx.android.synthetic.main.fragment_user_details.view.*
 
 
 class UserDetailsFragment : Fragment() {
@@ -44,6 +46,21 @@ class UserDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        val view: View = inflater!!.inflate(R.layout.fragment_user_details, container, false)
+
+        view.editUserInfoButton.setOnClickListener { view ->
+            Toast.makeText(activity, "Kliknięto przycisk", Toast.LENGTH_SHORT).show()
+            val intent = Intent(activity, EditUserDetailsActivity::class.java)
+            startActivity(intent)
+        }
+        view.editUserPasswordButton.setOnClickListener { view ->
+            Toast.makeText(activity, "Kliknięto przycisk", Toast.LENGTH_SHORT).show()
+            val intent = Intent(activity, EditUserPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
         return inflater.inflate(R.layout.fragment_user_details, container, false)
     }
 
